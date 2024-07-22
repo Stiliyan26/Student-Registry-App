e723f8e83a4f57ad223134f7430ade93a0a2eb8b
Stiliyan26 <stili262002@abv.bg>
2024-07-22 21:55:21 +0300

    update the title

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -2,7 +2,7 @@ function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
       title: "MVC Example",
-      msg: "Students Registry",
+      msg: "Welcome Test",
       students: students
     };
     res.render('home', model);
